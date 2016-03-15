865a9078d81c5a283a4a7ba88c56b825ab58e2b4
Joan Massich <sik@visor.udg.edu>
2016-03-15 10:00:59 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Mode strict de JavaScript
+"use strict";
+
+//Fonction principale du programme
+function main()
+{
+    
+}
+
+//Quand la page est complètement chargée, on commence
+$(window).ready(main);
